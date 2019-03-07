48d322ea8b1d0f4a95514049460274dec1073f40
Ana Silva <anasilva@lsd.ufcg.edu.br>
2019-03-07 12:30:21 -0300

    sunburst module refactoring continued: loading, data visualization (caption and description of layers) were separated from the main js file.

--- geonode/static/lib/js/sunburst-manager.js
+++ geonode/static/lib/js/sunburst-manager.js
@@ -1,111 +1,13 @@
 // (refactoring) TODO this constants must be setted in another file (Issue 3)    
     const GEOSERVER_URL = 'http://192.168.15.13/geoserver/ows/';
     const DEFAULT_LAYER_OPACITY = 0.85;
+    const CENTER_COORDS = [-4615573.515972, -794945.094166];
 
     changeAdvancedButton = (imgName) => {
       let url = window.location.protocol + '//' + window.location.hostname + '/maps/new?layer=' + imgName;
 
       $('#advanced-button').attr("href", url);
-    }
-
-    fillLegend = (imgName) => {
-      let legendContainerElement = $('#container-legend')
-      
-      if (imgName == null || imgName === '') {
-        legendContainerElement.hide();
-        return;
-      } else {
-        legendContainerElement.show();
-      }
-      const urlSufix = '?service=wms&request=GetLegendGraphic&format=image%2Fpng&width=20&height=20&layer=' + imgName;
-
-      // show loader
-      let legendLoadingElement = $('#legend-loader');
-      legendLoadingElement.show();
-      
-      const url = GEOSERVER_URL + urlSufix;
-      let legendElement = $('#legend-image');
-      legendElement.attr("src", url);
-
-      legendElement.on("load", () => {
-        // hidden loader
-        legendLoadingElement.hide();
-      });
-      legendElement.on("error", () => {
-        // hidden loader
-        legendLoadingElement.hide();
-      });      
-    }
-
-    fillBreadcrumbs = (map, sunburst, data, defaultL, aguasLayer, sabLayer, layerTitle) => {
-      $(".link-breadcrumbs").unbind();
-      let breadcrumbsArr = getNameArray(data.__dataNode, []);
-
-      let breadcrumbs_container = $("#breadcrumbs");
-      
-      breadcrumbs_container.empty();
-      breadcrumbsArr.reverse().forEach((value) => {
-        const linkId = 'link-breadcrumbs-' + Math.floor(Math.random() * 1000000);;
-        breadcrumbs_container.append('<span class="crumb"><a id="' + linkId + '" class="link-breadcrumbs" imgname="' + value.imgName  + '">' + value.title + '</a></span>');
-        
-        $(document).on('click', '#' + linkId, function (event) {
-          actionOnSunbust(map, sunburst, value.data, defaultL, aguasLayer, sabLayer, layerTitle)        
-        }); 
-      });
-
-    }
-
-    getNameArray = (data, array) => {
-      array = array || [];
-      array.push({title: data.data.name2, imgName: data.data.imgName, data: data.data});
-      if (data.parent) getNameArray(data.parent, array);
-      return array;
-    };
-
-    fillDescrition = (data) => {      
-      let titleContentEl = '#container-title-row-content';
-      let descriptionContentEl = '#container-description-row-content';
-      $(titleContentEl).text(data.name2 ? data.name2 : '-');
-      $(descriptionContentEl).text('');
-
-      let descriptionLoaderElement = $('#description-loader');
-      descriptionLoaderElement.show();
-
-      let url = window.location.protocol + '//' + window.location.hostname + '/layers/' + data.imgName + '/metadata_detail_rest';
-      $.get(url)
-      .done(function (data) {
-        $(titleContentEl).text(data.title);
-        $(descriptionContentEl).text(data.abstract);
-      })
-      .fail(function (error) {
-        $(titleContentEl).text(data.name2 ? data.name2 : '-');
-        $(descriptionContentEl).text('');
-      })
-      .always(function() {
-        descriptionLoaderElement.hide();
-      });
-    };
-
-    // TODO check. This "Loading" management might become a new js file
-    let layersLoadingCount = 0;
-    tryStartLoading = () => {
-      if (layersLoadingCount <= 0) {
-          $('#map-loader').show();
-        }
-        layersLoadingCount++;
-    }
-
-    tryEndLoading = () => {
-      layersLoadingCount > 0 && layersLoadingCount--;
-      if (layersLoadingCount == 0) {
-        $('#map-loader').hide();
-      }
-    }
-
-    tryForceEndLoading = () => {
-      $('#map-loader').hide();
-      layersLoadingCount = 0;
-    }    
+    }  
     
     let currentLayer;
     createLayer = (imgName) => {
@@ -154,33 +56,7 @@
       window.stop();
     }
 
-    actionOnSunbust = (map, sunburst, choosenData, defaultL, aguasLayer, sabLayer, layerTitle) => {
-      let imgName = choosenData.imgName;
-      if (currentLayer === imgName) {
-          console.log('The same image. Do nothing.')
-          return;
-        }
-        stopPeddingRequests();
-        map.getLayers().clear();
-        sunburst.focusOnNode(choosenData);
-        
-        let layer1 = createLayer(imgName);    
-
-        map.addLayer(defaultL);
-        map.addLayer(layer1);
-        map.addLayer(aguasLayer);
-        map.addLayer(sabLayer);
-        map.addLayer(layerTitle);
-        
-        changeAdvancedButton(choosenData.imgName);
-        fillBreadcrumbs(map, sunburst, choosenData, defaultL, aguasLayer, sabLayer, layerTitle);
-        fillLegend(choosenData.imgName);
-        fillDescrition(choosenData);      
-    }
-
-    let sunburst;
-      $(document).ready(() => {
-      const layerTitleSource = new ol.source.XYZ({
+    const layerTitleSource = new ol.source.XYZ({
         url: 'https://services.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Reference/MapServer/tile/{z}/{y}/{x}'
       })
 
@@ -225,13 +101,38 @@
       const initialImgNameLayer = dataDesertificacao.imgName;
       let rootLayer = createLayer(initialImgNameLayer);
 
-      // TODO (refactoring) put the center parameters in a constant (Issue 4)
+    actionOnSunbust = (map, sunburst, choosenData, defaultL, aguasLayer, sabLayer, layerTitle) => {
+      let imgName = choosenData.imgName;
+      if (currentLayer === imgName) {
+          console.log('The same image. Do nothing.')
+          return;
+        }
+        stopPeddingRequests();
+        map.getLayers().clear();
+        sunburst.focusOnNode(choosenData);
+        
+        let layer1 = createLayer(imgName);    
+
+        map.addLayer(defaultL);
+        map.addLayer(layer1);
+        map.addLayer(aguasLayer);
+        map.addLayer(sabLayer);
+        map.addLayer(layerTitle);
+        
+        changeAdvancedButton(choosenData.imgName);
+        fillBreadcrumbs(map, sunburst, choosenData, defaultL, aguasLayer, sabLayer, layerTitle);
+        fillLegend(choosenData.imgName);
+        fillDescrition(choosenData);      
+    }
+
+    let sunburst;
+      $(document).ready(() => {
       let map = new ol.Map({
         layers: [defaultL, rootLayer, aguasLayer, sabLayer, layerTitle],
         target: "map",
         pixelRatio: 1,
         view: new ol.View({
-          center: [-4615573.515972, -794945.094166],
+          center: CENTER_COORDS,
           zoom: 5
         })
       });
